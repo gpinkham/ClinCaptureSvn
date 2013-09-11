81d08df9cd81b043e0cd88324ac55bfda066d3e4
markg <markg@clinovo.com>
2013-09-11 14:31:17 +0000

    Refs #441: Add new test files
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@574 14881f01-568e-124b-961a-bb94932aa8d1

---
+++ core/src/test/java/org/akaza/openclinica/dao/admin/CRFDaoTest.java
@@ -0,0 +1,105 @@
+package org.akaza.openclinica.dao.admin;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.akaza.openclinica.DefaultAppContextTest;
+import org.akaza.openclinica.bean.admin.CRFBean;
+import org.akaza.openclinica.bean.core.Status;
+import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.exception.OpenClinicaException;
+import org.junit.Test;
+
+public class CRFDaoTest extends DefaultAppContextTest {
+
+	@Test
+	public void testThatCreateReturnsAnActiveCRF() throws OpenClinicaException {
+		
+		CRFBean crf = new CRFBean();
+		Date currentDate = new Date();
+
+		crf.setOid("F_END");
+		crf.setStudyId(1);
+		crf.setActive(false);
+		crf.setOwner((UserAccountBean) userAccountDAO.findByPK(1));
+		crf.setStatus(Status.AVAILABLE);
+		crf.setUpdater((UserAccountBean) userAccountDAO.findByPK(1));
+		crf.setName("End of Study");
+		crf.setDescription("Test");
+		crf.setCreatedDate(currentDate);
+		crf.setUpdatedDate(currentDate);
+		
+		crf = (CRFBean) crfdao.create(crf);
+		assertTrue(crf.isActive());
+	}
+
+	@Test
+	public void testThatFindByOidReturnsCRFWithCorrectOID() throws OpenClinicaException {
+		CRFBean crf = null;
+		String cfrOid = "F_CONC";
+
+		crf = crfdao.findByOid(cfrOid);
+		assertEquals(cfrOid, crf.getOid());
+	}
+
+	@Test
+	public void testThatFindByNameReturnsCRFWithCorrectName() throws OpenClinicaException {
+		CRFBean crf = null;
+		String cfrName = "Agent Administration";
+		
+		crf = (CRFBean) crfdao.findByName(cfrName);
+		assertEquals(cfrName, crf.getName());
+	}
+
+	@Test
+	public void testThatUpdateSetsTheChangedField() throws OpenClinicaException {
+		CRFBean crf = null;
+		String cfrOid = "F_AGEN";
+		String newDescription = "Test update";
+		
+		crf = (CRFBean) crfdao.findByOid(cfrOid);
+		crf.setDescription(newDescription);
+		crf.setUpdater((UserAccountBean) userAccountDAO.findByPK(1));
+		
+		crfdao.update(crf);
+
+		crf = crfdao.findByOid(cfrOid);
+		assertTrue(newDescription.equals(crf.getDescription()));
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testThatFindAllDoesNotReturnAnEmptyList() throws OpenClinicaException {
+		ArrayList<CRFBean> all;
+
+		all = (ArrayList<CRFBean>) crfdao.findAll();
+		assertFalse(all.isEmpty());
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testThatFindAllByStudyReturnsAllExistingCRFs() throws OpenClinicaException {
+		ArrayList<CRFBean> all;
+		int studyId = 1;
+
+		all = (ArrayList<CRFBean>) crfdao.findAllByStudy(studyId);
+		assertEquals(2, all.size());
+	}
+
+	@Test
+	public void testThatGetCountofActiveCRFsReturnsValidSize() throws OpenClinicaException {
+		Integer count = null;
+
+		count = crfdao.getCountofActiveCRFs();
+		assertTrue(count > 0);
+	}
+	
+	@Test
+	public void testThatFindByPKDoesNotReturnNull() throws OpenClinicaException {
+		CRFBean crf = null;
+		int CrfPK = 2;
+
+		crf = (CRFBean) crfdao.findByPK(CrfPK);
+		assertNotNull(crf);
+	}
+}
---
+++ core/src/test/java/org/akaza/openclinica/dao/submit/CRFVersionDAOTest.java
@@ -0,0 +1,119 @@
+package org.akaza.openclinica.dao.submit;
+
+import java.util.ArrayList;
+
+import org.akaza.openclinica.DefaultAppContextTest;
+import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.bean.submit.CRFVersionBean;
+import org.akaza.openclinica.exception.OpenClinicaException;
+import org.junit.Test;
+
+public class CRFVersionDAOTest extends DefaultAppContextTest {
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testThatFindAllByCRFReturnsAllCRFs() throws OpenClinicaException {
+		ArrayList<CRFVersionBean> all;
+		int cfrId = 1;
+
+		all = (ArrayList<CRFVersionBean>) crfVersionDao.findAllByCRF(cfrId);
+		assertEquals(1, all.size());
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testThatFindAllActiveByCRFReturnsAllCRFsAttachedToActiveStudy() throws OpenClinicaException {
+		ArrayList<CRFVersionBean> all;
+		int cfrId = 2;
+
+		all = (ArrayList<CRFVersionBean>) crfVersionDao.findAllActiveByCRF(cfrId);
+		assertEquals(2, all.size());
+	}
+	
+	@Test
+	public void testThatFindByPKDoesNotReturnNull() throws OpenClinicaException {
+		CRFVersionBean crfVer = null;
+		int PK = 1;
+
+		crfVer = (CRFVersionBean) crfVersionDao.findByPK(PK);
+		assertNotNull(crfVer);
+	}
+	
+	@Test
+	public void testThatFindByFullNameDoesNotReturnNull() throws OpenClinicaException {
+		CRFVersionBean crfVer = null;
+		String version = "1";
+		String cfrName = "Concomitant Medications AG";
+		
+		crfVer = (CRFVersionBean) crfVersionDao.findByFullName(version, cfrName);
+		assertNotNull(crfVer);
+	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testFindAllByOid() throws OpenClinicaException {
+		ArrayList<CRFVersionBean> all;
+		String oid= "F_CONC_V20";
+		
+		all = (ArrayList<CRFVersionBean>) crfVersionDao.findAllByOid(oid);
+		assertEquals(1, all.size());
+	}
+	
+	@Test
+	public void testThatGetCRFIdFromCRFVersionIdReturnsCorrectId() throws OpenClinicaException {
+		
+		assertEquals(2, crfVersionDao.getCRFIdFromCRFVersionId(2));
+	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testThatFindAllByCRFIdReturnsAllCRFVersionsAttachedToCRF() throws OpenClinicaException {
+		ArrayList<CRFVersionBean> all;
+		
+		all = (ArrayList<CRFVersionBean>) crfVersionDao.findAllByCRFId(2);
+		assertEquals(2, all.size());
+	}
+	
+	@Test
+	public void testThatFindCRFVersionIdReturnsCorrectCRFVersion() throws OpenClinicaException {
+		
+		assertEquals(new Integer(1), crfVersionDao.findCRFVersionId(1, "v2.0"));
+	}
+	
+	@Test
+	public void testThatFindByOidDoesNotReturnNull() throws OpenClinicaException {
+		CRFVersionBean crfVer = null;
+		String cfrVerOid = "F_AGEN_V20";
+		
+		crfVer = (CRFVersionBean) crfVersionDao.findByOid(cfrVerOid);
+		assertNotNull(crfVer);
+	}
+	
+	@Test
+	public void testThatUpdateSetsTheChangedFields() throws OpenClinicaException {
+		CRFVersionBean crfVer = null;
+		String cfrVerOid = "F_AGEN_V20";
+		String newDescription = "Test update";
+		
+		crfVer = (CRFVersionBean) crfVersionDao.findByOid(cfrVerOid);
+		crfVer.setDescription(newDescription);
+		crfVer.setUpdater((UserAccountBean) userAccountDAO.findByPK(1));
+		
+		crfVersionDao.update(crfVer);
+		
+		crfVer = (CRFVersionBean) crfVersionDao.findByOid(cfrVerOid);
+		assertTrue(newDescription.equals(crfVer.getDescription()));
+	}
+	
+	@Test
+	public void testThatDeleteRemovesSpecifiedVersionFromDB() throws OpenClinicaException {
+		int crfVerPK = 3;
+		CRFVersionBean crfVer = null;
+		
+		crfVersionDao.delete(crfVerPK);
+		
+		crfVer = (CRFVersionBean) crfVersionDao.findByPK(crfVerPK);
+		assertNull(crfVer.getOid());
+	}
+	
+}
