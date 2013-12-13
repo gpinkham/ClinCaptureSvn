48a10658127f8413965b6763f8e7f4c044e6ad0e
tom <tom@clinovo.com>
2013-12-13 13:07:34 +0000

    Fixes #831: Remove multithreading problems with unavailableCRFList Map - update to unit test. Submitted by sergey
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@889 14881f01-568e-124b-961a-bb94932aa8d1

--- web/src/test/java/org/akaza/openclinica/multithreading/UnavailableCRFListTest.java
+++ web/src/test/java/org/akaza/openclinica/multithreading/UnavailableCRFListTest.java
@@ -1,6 +1,8 @@
 package org.akaza.openclinica.multithreading;
 
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.List;
 
 import org.akaza.openclinica.control.core.Controller;
 import org.junit.Before;
@@ -12,8 +14,9 @@ public class UnavailableCRFListTest {
 
 	private static boolean errors;
 	private static boolean cmErrors;
-	private static final int totalUsers = 1000;
-	private static final int totalEcbIds = 1000;
+	private static final int TOTAL_USERS = 10;
+	private static final int TOTAL_ECB_IDS = 10;
+	private static final List<Thread> threadList = new ArrayList<Thread>();
 
 	private class ServletThread extends Thread {
 
@@ -26,16 +29,16 @@ public class UnavailableCRFListTest {
 		@Override
 		public void run() {
 			try {
-				for (int ecbId = 1; ecbId < totalEcbIds; ecbId++) {
+				for (int ecbId = 1; ecbId < TOTAL_ECB_IDS; ecbId++) {
 					Controller.lockThisEventCRF(ecbId, userId);
 				}
-                Controller.removeLockedCRF(userId);
-				for (int ecbId = 1; ecbId < totalEcbIds; ecbId++) {
-                    Controller.lockThisEventCRF(ecbId, userId);
+				Controller.removeLockedCRF(userId);
+				for (int ecbId = 1; ecbId < TOTAL_ECB_IDS; ecbId++) {
+					Controller.lockThisEventCRF(ecbId, userId);
 				}
-				for (int ecbId = 1; ecbId < totalEcbIds; ecbId++) {
+				for (int ecbId = 1; ecbId < TOTAL_ECB_IDS; ecbId++) {
 					if (Controller.getUnavailableCRFList().containsKey(ecbId)) {
-                        Controller.justRemoveLockedCRF(ecbId);
+						Controller.justRemoveLockedCRF(ecbId);
 					}
 				}
 			} catch (ConcurrentModificationException e) {
@@ -52,11 +55,26 @@ public class UnavailableCRFListTest {
 		//
 	}
 
+	private boolean areAllAlive() {
+		boolean result = false;
+		for (Thread thread : threadList) {
+			if (thread.isAlive()) {
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+
 	@Test
 	public void testThatOperationsWithUnavailableCRFLisDoNotThrowConcurrentModificationExceptions() throws Exception {
-		for (int userId = 1; userId < totalUsers; userId++) {
-			new ServletThread(userId).start();
+		for (int userId = 1; userId <= TOTAL_USERS; userId++) {
+			ServletThread servletThread = new ServletThread(userId);
+			threadList.add(servletThread);
+			servletThread.start();
 		}
+		while (areAllAlive())
+			Thread.sleep(1000);
 		assertFalse(errors);
 		assertFalse(cmErrors);
 	}
