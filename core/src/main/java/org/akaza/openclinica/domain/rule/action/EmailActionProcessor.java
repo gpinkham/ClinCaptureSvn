bdb77c2b9e60b311917916d55be9aea396f3e696
tom <tom@clinovo.com>
2014-03-28 12:31:19 +0000

    Fixes #1189: Email action triggers a discrepancy message and oops page. Submitted by sergey
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@1302 14881f01-568e-124b-961a-bb94932aa8d1

--- core/src/main/java/org/akaza/openclinica/domain/rule/action/EmailActionProcessor.java
+++ core/src/main/java/org/akaza/openclinica/domain/rule/action/EmailActionProcessor.java
@@ -1,18 +1,19 @@
 /*******************************************************************************
  * ClinCapture, Copyright (C) 2009-2013 Clinovo Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the Lesser GNU General Public License 
  * as published by the Free Software Foundation, either version 2.1 of the License, or(at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the Lesser GNU General Public License along with this program.  
  \* If not, see <http://www.gnu.org/licenses/>. Modified by Clinovo Inc 01/29/2013.
  ******************************************************************************/
 
 package org.akaza.openclinica.domain.rule.action;
 
+import com.clinovo.context.SubmissionContext;
 import org.akaza.openclinica.bean.login.UserAccountBean;
 import org.akaza.openclinica.bean.managestudy.StudyBean;
 import org.akaza.openclinica.bean.submit.ItemDataBean;
@@ -27,30 +28,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
-import com.clinovo.context.SubmissionContext;
-
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.sql.DataSource;
-
 @SuppressWarnings("unchecked")
 public class EmailActionProcessor implements ActionProcessor {
 
 	protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-	Connection connection;
-	DataSource ds;
-	EmailEngine emailEngine;
-	JavaMailSenderImpl mailSender;
-	RuleActionRunLogDao ruleActionRunLogDao;
-	DiscrepancyNoteService discrepancyNoteService;
-	RuleSetRuleBean ruleSetRule;
+	private Connection connection;
+	private DataSource ds;
+	private JavaMailSenderImpl mailSender;
+	private RuleActionRunLogDao ruleActionRunLogDao;
+	private DiscrepancyNoteService discrepancyNoteService;
+	private RuleSetRuleBean ruleSetRule;
 
 	public EmailActionProcessor(Connection connection, DataSource ds, JavaMailSenderImpl mailSender,
 			RuleActionRunLogDao ruleActionRunLogDao, RuleSetRuleBean ruleSetRule) {
@@ -70,11 +67,11 @@ public class EmailActionProcessor implements ActionProcessor {
 		}
 
 		case SAVE: {
-			HashMap<String, String> arg0 = (HashMap<String, String>) arguments[0];
-            if (!sendEmail(ruleAction, ub, arg0.get("body"), arg0.get("subject"))) {
+			HashMap<String, String> arg0 = (HashMap<String, String>) arguments[1];
+			if (!sendEmail(ruleAction, arg0.get("body"), arg0.get("subject"))) {
 				getDiscrepancyNoteService().saveFieldNotes(((EmailActionBean) ruleAction).getExceptionMessage(),
 						itemDataBean.getId(), itemData, connection, currentStudy, ub, true);
-            }
+			}
 			RuleActionRunLogBean ruleActionRunLog = new RuleActionRunLogBean(ruleAction.getActionType(), itemDataBean,
 					itemDataBean.getValue(), ruleSetRule.getRuleBean().getOid());
 			ruleActionRunLogDao.saveOrUpdate(ruleActionRunLog, connection);
@@ -85,7 +82,7 @@ public class EmailActionProcessor implements ActionProcessor {
 		}
 	}
 
-	private boolean sendEmail(RuleActionBean ruleAction, UserAccountBean ub, String body, String subject) {
+	private boolean sendEmail(RuleActionBean ruleAction, String body, String subject) {
 		boolean sent = true;
 		logger.info("Sending email...");
 		try {
@@ -116,7 +113,7 @@ public class EmailActionProcessor implements ActionProcessor {
 		int sizeTo = recipientsArray.size();
 		InternetAddress[] addressTo = new InternetAddress[sizeTo];
 		for (int i = 0; i < sizeTo; i++) {
-			addressTo[i] = new InternetAddress(recipientsArray.get(i).toString());
+			addressTo[i] = new InternetAddress(recipientsArray.get(i));
 		}
 		return addressTo;
 
