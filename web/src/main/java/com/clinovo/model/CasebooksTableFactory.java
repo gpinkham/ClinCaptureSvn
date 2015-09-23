33bc831ef03897b9f655b451a902409571624440
sergey <sergey@clinovo.com>
2015-09-23 13:42:41 +0000

    Fixes #2532: Site Dropdown filter does not work in CaseBooks page. Submitted by denis
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@3116 14881f01-568e-124b-961a-bb94932aa8d1

--- web/src/main/java/com/clinovo/model/CasebooksTableFactory.java
+++ web/src/main/java/com/clinovo/model/CasebooksTableFactory.java
@@ -1,12 +1,15 @@
 package com.clinovo.model;
 
+import org.akaza.openclinica.bean.core.Status;
 import org.akaza.openclinica.bean.managestudy.StudySubjectBean;
 import org.akaza.openclinica.bean.submit.SubjectBean;
 import org.akaza.openclinica.control.AbstractTableFactory;
 import org.akaza.openclinica.control.DefaultActionsEditor;
 import org.akaza.openclinica.dao.submit.SubjectDAO;
+import org.akaza.openclinica.web.table.sdv.SDVSimpleListFilter;
 import org.jmesa.facade.TableFacade;
 import org.jmesa.view.component.Row;
+import org.jmesa.view.editor.BasicCellEditor;
 import org.jmesa.view.editor.CellEditor;
 import org.springframework.context.MessageSource;
 
@@ -66,15 +69,14 @@ public class CasebooksTableFactory extends AbstractTableFactory {
 
         Row row = tableFacade.getTable().getRow();
         configureColumn(row.getColumn(SELECT), getMessageSource().getMessage("select", null, locale), new SelectCellEditor(), new DefaultActionsEditor(locale), true, false);
-        configureColumn(row.getColumn(SITE_NAME), getMessageSource().getMessage("site_name", null, locale), null, null, true, true);
+        configureColumn(row.getColumn(SITE_NAME), getMessageSource().getMessage("site_name", null, locale), null, new SDVSimpleListFilter(getSitesOIDsList()), true, true);
         configureColumn(row.getColumn(STUDY_SUBJECT_ID), getMessageSource().getMessage("study_subject_ID", null, locale), null, null, true, true);
         configureColumn(row.getColumn(DATE_CREATED), getMessageSource().getMessage("date_created", null, locale), null, null, true, true);
-        configureColumn(row.getColumn(SUBJECT_STATUS), getMessageSource().getMessage("subject_status", null, locale), null, null, true, true);
+        configureColumn(row.getColumn(SUBJECT_STATUS), getMessageSource().getMessage("subject_status", null, locale), new BasicCellEditor(), null, true, true);
         configureColumn(row.getColumn(SEX), getMessageSource().getMessage("gender", null, locale), null, null, true, true);
         configureColumn(row.getColumn(STUDY_SUBJECT_SECOND_LABEL), getMessageSource().getMessage("secondary_label", null, locale), null, null, true, true);
         configureColumn(row.getColumn(DATE_OF_BIRTH), getMessageSource().getMessage("date_of_birth", null, locale), null, null, true, true);
         configureColumn(row.getColumn(PERSON_ID), getMessageSource().getMessage("subject_unique_ID", null, locale), null, null, true, true);
-
     }
 
     @Override
@@ -89,7 +91,7 @@ public class CasebooksTableFactory extends AbstractTableFactory {
             h.put(DATE_CREATED, studySubjectBean.getCreatedDate());
             h.put(DATE_OF_BIRTH, subjectBean.getDateOfBirth());
             h.put(PERSON_ID, isCopyPersonId() ? studySubjectBean.getId() : subjectBean.getUniqueIdentifier());
-            h.put(SUBJECT_STATUS, studySubjectBean.getStatus().getName());
+            h.put(SUBJECT_STATUS, getColoredStatus(studySubjectBean.getStatus()));
             h.put(SEX, studySubjectBean.getGender());
             h.put(SITE_NAME, studySubjectBean.getStudyName());
             h.put("studySubjectOid", studySubjectBean.getOid());
@@ -113,6 +115,16 @@ public class CasebooksTableFactory extends AbstractTableFactory {
     public void setStudySubjectBeanList(List<StudySubjectBean> studySubjectBeanList) {
         this.studySubjectBeanList = studySubjectBeanList;
     }
+
+	private List<String> getSitesOIDsList() {
+		List<String> oidList = new ArrayList<String>();
+		if (studySubjectBeanList != null) {
+			for (StudySubjectBean subject : studySubjectBeanList) {
+				oidList.add(subject.getStudyName());
+			}
+		}
+		return oidList;
+	}
 
     public List<StudySubjectBean> getStudySubjectBeanList() {
         return studySubjectBeanList;
@@ -185,8 +197,13 @@ public class CasebooksTableFactory extends AbstractTableFactory {
     private class SelectCellEditor implements CellEditor {
 		public Object getValue(Object o, String s, int i) {
             String studySubjectOid = (String) ((HashMap<Object, Object>) o).get("studySubjectOid");
-            String html = "<div style=\"width:130px;text-align:center;\"><input name=\"oids\" type=\"checkbox\" ssOid=\"" + studySubjectOid + "\"/></div>";
-            return html;
+			return "<div style=\"width:130px;text-align:center;\"><input name=\"oids\" type=\"checkbox\" ssOid=\"" + studySubjectOid + "\"/></div>";
         }
     }
+
+	private String getColoredStatus(Status status) {
+		String className = status == Status.AVAILABLE ? "aka_green_highlight"
+				: status == Status.AUTO_DELETED || status == Status.DELETED ? "aka_red_highlight" : "";
+		return "<span class=" + className + ">" + status.getName() + "</span>";
+	}
 }
