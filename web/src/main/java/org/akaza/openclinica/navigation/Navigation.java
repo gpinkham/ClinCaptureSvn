15781c0fbbcc37b5a3922a509bab0e17a88d5309
oleg <oleg@clinovo.com>
2016-01-08 13:19:52 +0000

    Fixed #2772: Exclude DeleteEventDefinitionCRF from Back logic. Submitted by: denis
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@3375 14881f01-568e-124b-961a-bb94932aa8d1

--- web/src/main/java/org/akaza/openclinica/navigation/Navigation.java
+++ web/src/main/java/org/akaza/openclinica/navigation/Navigation.java
@@ -45,7 +45,7 @@ public final class Navigation {
 			"/RestoreCRF", "/RestoreCRFVersion", "/pages/deleteCRFVersion", "/LockCRFVersion", "/UnlockCRFVersion",
 			"/CreateSubStudy", "/RemoveCRFFromDefinition", "/RestoreCRFFromDefinition", "/AddCRFToDefinition",
 			"/AdministrativeEditing", "/pages/completeCRFDelete", "/pages/configureItemLevelSDV",
-			"/pages/deleteEventDefinition"));
+			"/pages/deleteEventDefinition", "/pages/deleteEventDefinitionCRF"));
 	// ignored-set of pages, pop-ups or like pop-ups
 	private static Set<String> exclusionPopUpURLs = new HashSet<String>(Arrays.asList("/ViewStudySubjectAuditLog",
 			"/PrintDataEntry", "/DiscrepancyNoteOutputServlet", "/PrintDataEntry",
