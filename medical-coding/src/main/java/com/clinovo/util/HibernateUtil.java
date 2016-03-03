6b6525857584204e3952a7c023edc5fa49fcf1f6
oleg <oleg@clinovo.com>
2016-03-03 11:15:03 +0000

    Fixed #2923: Create new default for MEDDRA = MEDDRA-19. Submitted by: denis
    git-svn-id: https://svn.clinovo.com/clincapture/trunk@3548 14881f01-568e-124b-961a-bb94932aa8d1

--- medical-coding/src/main/java/com/clinovo/util/HibernateUtil.java
+++ medical-coding/src/main/java/com/clinovo/util/HibernateUtil.java
@@ -49,7 +49,7 @@ public final class HibernateUtil {
     private static SessionFactory sessionFactory;
 
     private static final String WHOD_DEFAULT = "WHOD-0915";
-    private static final String MEDDRA_DEFAULT = "MEDDRA-171";
+    private static final String MEDDRA_DEFAULT = "MEDDRA-19";
 
     /**
      * Returns session for term search.
